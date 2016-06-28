6e3afd5d186c69ff857efde92307f64bff8d6e6f
Timothy Minahan <timothy@minahan.net>
2016-06-29 09:05:36 +0930

    INitial Arcane Mage Macro

--- GS-DraikMacros/Macros/Mage.lua
+++ GS-DraikMacros/Macros/Mage.lua
@@ -3,74 +3,32 @@ local Sequences = GSMasterSequences
 ------------------
 ----- Mage
 ------------------
-
+-- Fire 63
+-- Frost 64
 
 Sequences['Arcaneaoe'] = { 
-  specID = 62,
-  author = "Draik",
-  helpTxt = "AoE",
-StepFunction = GSStaticPriority, 
-  PreMacro = [[
-/targetenemy [noharm][dead]
 
-]],
-  '/castsequence reset=combat Arcane Explosion, Arcane Explosion, Arcane Explosion, Arcane Explosion, Arcane Barrage',
-  '/cast Supernova',
-  '/cast Arcane Power',
-  '/cast Presense of Mind',
-  PostMacro = [[
-/startattack
-/use [combat]13
-/use [combat]14
-]],
-}
 
-Sequences['Arcanest'] = { 
-  specID = 62,
-  author = "Draik",
-  helpTxt = "Single Target",
-StepFunction = GSStaticPriority,
-  PreMacro = [[
-/targetenemy [noharm][dead]
 
-]],
-  '/castsequence reset=combat Arcane Blast',
-  '/cast [nochanneling] Arcane Missiles',
-  '/cast Supernova',
-  '/cast Arcane Power',
-  '/cast Presense of Mind',
-  PostMacro = [[
-/startattack
-/use [combat]13
-/use [combat]14
-]],
-}
 
-Sequences['Frosty'] = {
-  specID = 64,
-  author = "Draik",
-  helpTxt = "Frost Single Target - Talents 3,3,2,2,3,1,3",
+------------------------
+Sequences['ArcaneLegion'] = { 
+specID = 62,
+author = "Flashgreer - wowlazymacros.com",
+helpTxt = "2122132",
 PreMacro = [[
 /targetenemy [noharm][dead]
 ]],
-[[/castsequence reset=25, Ice Barrier]],
-[[/castsequence reset=20, Ice Ward]],
-[[/castsequence reset=30, Comet Storm]],
-[[/castsequence reset=25, Ice Nova,Frostbolt,Frostbolt,Frostbolt,Ice Lance,Ice Lance,Frostfire Bolt,Frostbolt,Frostbolt,Frostbolt,Ice Lance,Ice Lance,Frostfire Bolt]],
-[[/cast Frozen Orb]],
-[[/cast Ice Nova]],
-[[/castsequence reset=20 Deep Freeze,Ice Lance,Ice Lance]],
-
+'/castsequence [nochanneling]Arcane Blast,Arcane Blast,Arcane Blast,Arcane Blast,Arcane Barrage',
+'/cast [nochanneling]Arcane Missiles',
+'/castsequence [nochanneling]charged up, Arcane Barrage',
+'/cast [nochanneling]Rune of power',
 PostMacro = [[
 /startattack
-/use [combat]13
-/use [combat]14
-/use [combat]Ice Floes
-/use [combat]Icy Veins
-/use [combat]Mirror Image
-/use [combat]Frozen Orb
-
+/cast [combat]Arcane Power
+/cast [combat]Presence of Mind
+/script UIErrorsFrame:Hide();
+/console Sound_EnableSFX 1
 ]],
 }
-
 
