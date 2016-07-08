d4a1c933b90fd695bb245dd113ab7233a4341870
Timothy Minahan <timothy@minahan.net>
2016-07-08 16:49:34 +0930

    Disc and Holy DPS Macros

--- GS-DraikMacros/Macros/Priest.lua
+++ GS-DraikMacros/Macros/Priest.lua
@@ -21,3 +21,50 @@ PostMacro = [[
 /use [combat]14
 ]],
 }
+
+Sequences['DiscDeeps'] = {
+specID = 256,
+author = "Draik",
+helpTxt = "Talents 3113131",
+icon = "Ability_Mage_FireStarter",
+PreMacro = [[
+/targetenemy [noharm][dead]
+]],
+'/cast Smite',
+'/cast [nochanneling] Penance',
+'/cast Halo',
+'/cast Holy Nova',
+'/cast Purge the Wicked',
+'/cast Mindbender',
+'/cast Schism',
+'/cast Shining Force',
+PostMacro = [[
+/startattack
+/use [combat]13
+/use [combat]14
+]],
+}
+
+
+
+
+Sequences['HolyPriesty'] = {
+specID = 257,
+author = "Draik",
+helpTxt = "Talents 3121133",
+icon = "Ability_Priest_Archangel",
+PreMacro = [[
+/targetenemy [noharm][dead]
+]],
+'/cast Smite',
+'/cast Holy Fire',
+'/cast Halo',
+'/cast Holy Nova',
+'/cast Holy Word:Chastise',
+PostMacro = [[
+/startattack
+/use [combat]13
+/use [combat]14
+]],
+}
+
