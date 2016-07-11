67b750e9a635778fd654ae4a45b225a103d17d9f
Timothy Minahan <timothy@minahan.net>
2016-07-11 17:55:33 +0930

    Cleanup for #3
    Still need a way to convert the string back to a table.

--- GS-SequenceEditor/core.lua
+++ GS-SequenceEditor/core.lua
@@ -1,7 +1,6 @@
 GSSE = LibStub("AceAddon-3.0"):NewAddon("GSSE", "AceConsole-3.0")
 local AceGUI = LibStub("AceGUI-3.0")
 
-
 -- Create Dialog
 
 local textstore  -- temporary text box on the dialog used for input.
@@ -10,23 +9,26 @@ local frame = AceGUI:Create("Frame")
 frame:SetTitle("Sequence Editor")
 frame:SetStatusText("Gnome Sequencer: Sequence Editor")
 
-frame:SetCallback("OnClose", function(widget) AceGUI:Release(widget) end)
-frame:SetLayout("Flow")
+frame:SetCallback("OnClose", function(widget) frame:Hide() end)
+frame:SetLayout("List")
 
 local editbox = AceGUI:Create("EditBox")
 editbox:SetLabel("Load Sequence")
 editbox:SetWidth(200)
-editbox:SetCallback("OnEnterPressed", function(widget, event, text) textstore = text end)
+editbox:DisableButton(true)
 frame:AddChild(editbox)
 
 local button = AceGUI:Create("Button")
 button:SetText("Load")
 button:SetWidth(200)
-button:SetCallback("OnClick", function() GSSE:loadSequence(textstore) end)
+button:SetCallback("OnClick", function() GSSE:loadSequence(editbox:GetText()) end)
 frame:AddChild(button)
 
 local sequencebox = AceGUI:Create("MultiLineEditBox")
-sequencebox:SetLabel("Load Sequence")
+sequencebox:SetLabel("Sequence")
+sequencebox:SetNumLines(20)
+sequencebox:DisableButton(true)
+sequencebox:SetFullWidth(true)
 frame:AddChild(sequencebox)
 
 local updbutton = AceGUI:Create("Button")
@@ -51,11 +53,11 @@ end
 function GSSE:updateSequence(sequenceText)
     
     local sequenceIndex = GetMacroIndexByName("LiveTest")
-    GSMasterSequences["LiveTest"] = sequenceText
+    GSMasterSequences["LiveTest"] = GSSE:lines(sequenceText)
     GSMasterSequences["LiveTest"].author = GetUnitName("player", true) .. '@' .. GetRealmName()
-    GSMasterSequences["LiveTest"].specID = getSpecID()
-    GSMasterSequences["LiveTest"].helpTxt = "Talents: " .. getCurrentTalents()
-    GSMasterSequences["LiveTest"].icon = getMacroIcon(sequenceIndex)
+    GSMasterSequences["LiveTest"].specID = GSSE:getSpecID()
+    GSMasterSequences["LiveTest"].helpTxt = "Talents: " .. GSSE:getCurrentTalents()
+    GSMasterSequences["LiveTest"].icon = GSSE:getMacroIcon(sequenceIndex)
     GSUpdateSequence("LiveTest", GSMasterSequences["LiveTest"])
 
     loadSequence("LiveTest")
@@ -80,7 +82,7 @@ function GSSE:OnInitialize()
     --frame:Hide()
 end
 
-local function getCurrentTalents()
+function GSSE:getCurrentTalents()
   local talents = ""
   for talentTier = 1, MAX_TALENT_TIERS do
     local available, selected = GetTalentTierInfo(talentTier, 1)
@@ -89,20 +91,28 @@ local function getCurrentTalents()
   return talents
 end
 
-local function getSpecID()
+function GSSE:getSpecID()
     local currentSpec = GetSpecialization()
     return currentSpec and select(1, GetSpecializationInfo(currentSpec)) or "None"
 end
 
-local function getMacroIcon(sequenceIndex)
-  if isempty(sequenceIndex) then
+function GSSE:getMacroIcon(sequenceIndex)
+  if GSSE:isempty(sequenceIndex) then
     local _, _, _, specicon, _, _, _ = GetSpecializationInfoByID(getSpecID())
     return strsub(specicon, 17)  
   else
-    return GetMacroIconInfo(sequenceIndex)
+    local _, iconpath, _ =  GetMacroInfo(sequenceIndex)
+    return iconpath
   end
 end
 
-local function isempty(s)
+function GSSE:isempty(s)
   return s == nil or s == ''
+end
+
+function GSSE:lines(str)
+  local t = {}
+  local function helper(line) table.insert(t, line) return "" end
+  helper((str:gsub("(.-)\r?\n", helper)))
+  return t
 end
