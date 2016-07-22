44b4a76907a897b03d0a181225211a0d5b7592f3
Timothy Minahan <timothy@minahan.net>
2016-07-22 15:21:14 +0930

    KillTheNoise' Disc Macros

--- GS-DraikMacros/Macros/Priest.lua
+++ GS-DraikMacros/Macros/Priest.lua
@@ -45,7 +45,37 @@ PostMacro = [[
 ]],
 }
 
+Sequences['DB_KTN_DiscDeeps'] = {
+specID = 256,
+author = "KTN",
+helpTxt = "Talents 3213131 - Set yourself as Focus.  ALso use the DB_KTN_Mouseover macro for some out of combat/dont pull things healing",
+icon = "Ability_Mage_FireStarter",
+PreMacro = [[
+/targetenemy [noharm][dead]
+]],
+'/cast [@focus] Power Word: Shield',
+'/cast Smite',
+'/cast [nochanneling] Penance',
+'/cast Halo',
+'/cast Holy Nova',
+'/cast Purge the Wicked',
+'/cast Mindbender',
+'/cast Schism',
+'/cast Shining Force',
+PostMacro = [[
+/startattack
+/castsequence [target=mouseover,help,nodead][] Plea, Shadow Mend
+/use [combat]13
+/use [combat]14
+]],
+}
 
+Sequences['DB_KTN_MouseOver'] = {
+specID = 256,
+author = "KTN",
+helpTxt = "Talents 3213131",
+'/castsequence [target=mouseover,help,nodead][] Power Word: Shield, Plea, Shadow Mend, Shadow Mend',
+}
 
 
 Sequences['DB_HolyPriesty'] = {
