9acf4e7515161dff08e1153d16670bd451d9ee1f
Timothy Minahan <timothy@minahan.net>
2016-07-22 18:31:54 +0930

    Subtlty and Assassination Sequences

--- GS-DraikMacros/Macros/Rogue.lua
+++ GS-DraikMacros/Macros/Rogue.lua
@@ -28,3 +28,53 @@ PostMacro = [[
 /use [combat] 14
 ]],
 }
+
+Sequences['DB_Assassin'] = {
+author='TimothyLuke',
+specID=259,
+helpTxt = 'Talents: 3113231',
+PreMacro=[[
+/targetenemy [noharm][dead]
+/cast [nostealth,nocombat]Stealth
+]],
+icon='Ability_Rogue_DeadlyBrew',
+"/cast [@focus] Tricks of the Trade",
+"/cast Rupture",
+"/cast Vendetta",
+"/cast Vanish",
+"/cast Hemorrhage",
+"/cast Garrote",
+"/cast Exsanguinate",
+"/cast Envenom",
+"/cast Mutilate",
+PostMacro=[[
+/use [combat] 13
+/use [combat] 14
+]],
+}
+
+Sequences['DB_Subtle'] = {
+author='TimothyLuke',
+specID=261,
+helpTxt = 'Talents: 1233212',
+PreMacro=[[
+/targetenemy [noharm][dead]
+/cast [nostealth,nocombat]Stealth
+/cast [combat] Marked for Death
+]],
+icon='Ability_Stealth',
+"/cast [@focus] Tricks of the Trade",
+"/cast Symbols of Death",
+"/cast Shadowstrike",
+"/cast Shadow Blades",
+"/cast Vanish",
+"/cast Nightblade",
+"/cast Shadow Dance",
+"/cast Shuriken Storm",
+"/cast Eviscerate",
+"/cast Backstab",
+PostMacro=[[
+/use [combat] 13
+/use [combat] 14
+]],
+}
