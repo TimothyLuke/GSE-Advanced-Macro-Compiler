6f10fa3e15b368a3d3101935997ab91c49bb1f93
Timothy Minahan <timothy@minahan.net>
2016-07-30 11:18:52 +0930

    HP_enhST from Rocktris added to HP Macros

--- GS-HighPerformanceMacros/Macros/Shaman.lua
+++ GS-HighPerformanceMacros/Macros/Shaman.lua
@@ -3,3 +3,25 @@ local Sequences = GSMasterSequences
 ------------------
 ----- Shaman
 ------------------
+Sequences['HP_enhST'] = {
+author = "Rocktris",
+helpTxt = "Talents are 3212112",
+specID = 263,
+lang="enUS",
+StepFunction = GSStaticPriority,
+PreMacro = [[
+/targetenemy [noharm][dead]
+]],
+'/castsequence Boulderfist',
+'/cast Stormstrike',
+'/cast Crash Lightning',
+'/castsequence Flametongue',
+'/cast Feral Spirit',
+PostMacro = [[
+/startattack
+/use [combat] 11
+/use [combat] 12
+/cast [combat] Doom Winds
+/cast [combat] Astral Shift
+]],
+}
