35645c0974509edfa189bdb057520e7a39a2d5c2
Timothy Minahan <timothy@minahan.net>
2016-08-02 19:55:21 +0930

    #25 Even More unittesting

--- spec/gs-core_spec.lua
+++ spec/gs-core_spec.lua
@@ -1,12 +1,15 @@
 
 describe('gs-core', function()
 
-
-  it('Check isempty', function()
+  setup(function()
     local t = {
       seterrorhandler = function(msg) print(msg) end
     }
     stub(t, "seterrorhandler")
+  end)
+
+  it('Check isempty', function()
+
     --local startup = require 'GS-Core/startup'
     local core = require 'GS-Core/Core'
     assert.equal(true, core._isempty(nil))
