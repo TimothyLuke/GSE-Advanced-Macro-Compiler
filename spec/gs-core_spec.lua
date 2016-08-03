1edab0ff77eeb3a29568e0e07269d7b0a2ab7050
Timothy Minahan <timothy@minahan.net>
2016-08-03 16:51:22 +0930

    #25 attempt z

--- spec/gs-core_spec.lua
+++ spec/gs-core_spec.lua
@@ -1,14 +1,26 @@
 
 describe('gs-core', function()
+  local LuaUnit = require('luaunit')
+  local mockagne = require('mockagne')
+  local wowmock = require('wowmock')
+  local when, verify = mockagne.when, mockagne.verify
+  -- Mocks
+  local globals, addon
+
+  setup(function()
+    -- Prepare an addon mock
+    addon = mockagne:getMock()
+
+    -- Prepare a globals mock
+    globals = mockagne:getMock()
+
+    -- Load the file to test
+      wowmock("GS-Core/Core.lua", globals, "GS-Core", addon)
+    end)
 
 
   it('Check isempty', function()
-    dofile("spec/wowmock/wowlua.lua")
-    dofile("spec/wowmock/wowmock.lua")
-    dofile("GS-Core/startup.lua")
-    dofile("GS-Core/errorhandler.lua")
-    --local startup = require 'GS-Core/startup'
-    local core = require 'GS-Core/Core'
-    assert.equal(true, core._isempty(nil))
+
+    assert.equal(true, addon._isempty(nil))
   end)
 end)
