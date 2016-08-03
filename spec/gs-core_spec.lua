8b2c1ce1b6edbc7c1f1894c804aa38bbff4d74ea
Timothy Minahan <timothy@minahan.net>
2016-08-03 13:08:52 +0930

    #25 corrected paths

--- spec/gs-core_spec.lua
+++ spec/gs-core_spec.lua
@@ -5,8 +5,8 @@ describe('gs-core', function()
   it('Check isempty', function()
 
     --dofile("spec/wowmock/wowmock.lua")
-    dofile("../GS-Core/startup.lua")
-    dofile(":../GS-Core/errorhandler.lua")
+    dofile("GS-Core/startup.lua")
+    dofile("GS-Core/errorhandler.lua")
     --local startup = require 'GS-Core/startup'
     local core = require 'GS-Core/Core'
     assert.equal(true, core._isempty(nil))
