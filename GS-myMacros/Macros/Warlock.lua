0badb7680e4bec779c9822e891b0fc6f3a3ef957
Timothy Minahan <timothy@minahan.net>
2016-10-27 10:11:18 +1100

    Retiring myMacros #111

--- GS-myMacros/Macros/Warlock.lua
+++
@@ -1,9 +0,0 @@
-local Sequences = GSMasterSequences -- Dont remove this
-------------------
--- Warlock
--- 9 - Classid
--- Spec ID’s
--- 265 - Affliction
--- 266 - Demonology
--- 267 - Destruction
--- Edit below this line  ---------------------
