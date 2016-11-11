e1047bd6a986bb1a8efae2407fbb4c97c52e850c
Timothy Minahan <timothy@minahan.net>
2016-11-11 15:19:09 +1030

    Druid for #123

--- GSE/SampleMacros/DeathKnight.lua
+++ GSE/SampleMacros/DeathKnight.lua
@@ -8,115 +8,77 @@ local Sequences = Statics.SampleMacros[6]
 ----- Death Knight
 ------------------
 
-Sequences['SAM_Blood'] = {
-specID = 250,
-author = "John Mets",
-helpTxt = "As for talents (2212213) but does run real smooth if you have Heartbreaker, Soulgorge and Ossuary.",
-StepFunction = "Priority",
-KeyPress = [[
-/Cast [combat] Vampiric Blood
-/Cast [combat] Dancing Rune Weapon
-]],
-'/cast Marrowrend',
-"/castsequence reset=combat Death's Caress, Blood Boil, Death Strike, blood Boil, Marrowrend",
-"/castsequence reset=combat Death's Caress, Blood Boil, Blood Boil, death strike, soulgorge",
-"/cast Heart Strike",
-"/cast Death Strike",
-KeyRelease = [[
-/cast [mod:alt] Anti-Magic Shell
-/TargetEnemy [noharm][dead]
-]],
-}
-
-Sequences['SAM_DBFrost'] = {
-specID = 251,
-author = "Suiseiseki",
-helpTxt = "Talents: 2132113",
-StepFunction = "Priority",
-KeyPress = [[
-/use [combat] Obliteration
-/use [combat] Pillar Of Frost
-]],
-'/cast [combat] Glacial Advance',
-'/cast Frost Strike',
-"/cast [combat] Remorseless Winter",
-'/castsequence Howling Blast, Frostscythe, Howling Blast, Obliterate',
-'/castsequence Howling Blast, Howling Blast, Frostscythe, Howling Blast, Obliterate',
-KeyRelease = [[
-]],
-}
-
 Sequences['SAM_FDK2'] = {
-specID = 251,
-author = "Tazkilla",
-helpTxt = "Talents:1111131",
-StepFunction = "Priority",
-KeyPress = [[
-/cast [combat] Pillar of Frost
-/cast [combat] Anti-Magic Shell
-]],
-'/castsequence reset=combat Frost Strike',
-'/cast Obliterate',
-'/cast Obliteration',
-'/castsequence reset=combat Howling Blast, Howling Blast, Howling Blast, Obliterate',
-'/castsequence reset=combat Howling Blast, Howling Blast, Howling Blast, Howling Blast',
-KeyRelease = [[
-/targetenemy [noharm][dead]
-]],
+  SpecID = 251,
+  Author = "Tazkilla",
+  Talents = "1,1,1,1,1,3,1",
+  Default=1,
+  MacroVersions = {
+    [1] = {
+      KeyPress = {
+        "/targetenemy [noharm][dead]",
+        "/cast [combat] Pillar of Frost",
+        "/cast [combat] Anti-Magic Shell"
+      },
+      StepFunction = "Priority",
+      '/castsequence reset=combat Frost Strike',
+      '/cast Obliterate',
+      '/cast Obliteration',
+      '/castsequence reset=combat Howling Blast, Howling Blast, Howling Blast, Obliterate',
+      '/castsequence reset=combat Howling Blast, Howling Blast, Howling Blast, Howling Blast',
+      KeyRelease = {
+        "/startattack",
+      },
+    },
+  }
 }
 
-Sequences['SAM_DF'] = {
-specID = 251,
-author = "John Mets",
-helpTxt = "Talents 2133121",
-StepFunction = "Priority",
-KeyPress = [[
-/cast [combat] Pillar of Frost
-]],
-"/castsequence reset=combat Obliterate, Frost Strike",
-'/castsequence reset=combat Obliterate, Frost Strike, Frost Strike, Obliterate, howling blast',
-"/castsequence reset=combat Obliteration",
-"/cast [combat] remorseless winter",
-"/cast [combat] empower rune weapon",
-KeyRelease = [[
-/targetenemy [noharm][dead]
-/cast [combat] Anti-Magic Shell
-]],
-}
 
 Sequences['SAM_DKunholy'] = {
-KeyPress = [[
-/targetenemy [noharm][dead]
-]],
-specID = 252,
-author = "throwryuken",
-helpTxt = "Talents 2221213",
-'/cast [nochanneling] Raise Dead',
-'/cast [nochanneling] Outbreak',
-'/cast [nochanneling] Dark Transformation',
-'/cast [nochanneling] Festering Strike',
-'/cast [nochanneling] Scourge Strike',
-'/cast [nochanneling] Soul Reaper',
-'/cast [nochanneling] Death Strike',
-'/cast [nochanneling] Summon Gargoyle',
-'/cast [nochanneling] Death Coil',
-KeyRelease = [[
-/startattack
-]],
+  SpecID = 252,
+  Author = "throwryuken",
+  Talents = "2,2,2,1,2,1,3",
+  Default=1,
+  MacroVersions = {
+    [1] = {
+      KeyPress = {
+        "/targetenemy [noharm][dead]",
+      },
+      '/cast [nochanneling] Raise Dead',
+      '/cast [nochanneling] Outbreak',
+      '/cast [nochanneling] Dark Transformation',
+      '/cast [nochanneling] Festering Strike',
+      '/cast [nochanneling] Scourge Strike',
+      '/cast [nochanneling] Soul Reaper',
+      '/cast [nochanneling] Death Strike',
+      '/cast [nochanneling] Summon Gargoyle',
+      '/cast [nochanneling] Death Coil',
+      KeyRelease = {
+        "/startattack",
+      },
+    },
+  }
 }
 
+
 Sequences['SAM_BloodDK'] = {
-StepFunction = "Priority",
-specID = 250,
-author = "Owns",
-helpTxt = "Talents 2112333",
-KeyPress = [[
-]],
-"/cast Marrowrend",
-"/castsequence reset=combat Death's Caress, Death Strike, Death Strike, Death Strike, Death Strike, Death Strike, Death Strike, Death Strike",
-'/castsequence reset=combat Blood Boil, Blood Boil, Marrowrend',
-'/castsequence reset=combat Heart Strike, Heart Strike, Heart Strike, Heart Strike, Marrowrend',
-KeyRelease = [[
-/targetenemy [noharm][dead]
-]],
+  StepFunction = "Priority",
+  SpecID = 250,
+  Author = "Owns",
+  Talents = "2,1,1,2,3,3,3",
+  Default=1,
+  MacroVersions = {
+    [1] = {
+      KeyPress = {
+        "/targetenemy [noharm][dead]",
+      },
+      "/cast Marrowrend",
+      "/castsequence reset=combat Death's Caress, Death Strike, Death Strike, Death Strike, Death Strike, Death Strike, Death Strike, Death Strike",
+      '/castsequence reset=combat Blood Boil, Blood Boil, Marrowrend',
+      '/castsequence reset=combat Heart Strike, Heart Strike, Heart Strike, Heart Strike, Marrowrend',
+      KeyRelease = {
+        "/startattack",
+      },
+    },
+  }
 }
--- GSE/SampleMacros/Druid.lua
+++ GSE/SampleMacros/Druid.lua
@@ -12,231 +12,155 @@ local Sequences = Statics.SampleMacros[11]
 --Feral 103
 --Balance 102
 
-Sequences['SAM_bear1'] = {
-specID = 104,
-author = "John Mets www.wowlazymacros.com",
-helpTxt = " Talents: 2331111",
-StepFunction = "Priority",
-KeyPress = [[
-/targetenemy [noharm][dead]
-]],
-"/castsequence reset=combat Thrash, Moonfire, Maul, Swipe",
-"/castsequence reset=combat Savage Defense, Swipe, Swipe, Savage Defense ,Frenzied Regeneration, Ironfur",
-"/cast Survival Instincts",
-"/cast Thrash",
-"/castsequence reset=combat Swipe, Moonfire, Maul, Mangle, Ironfur",
-"/cast Pulverize",
-"/cast Incapacitating Roar",
-"/cast [combat] Barkskin",
-"/cast [combat] Mighty Bash",
-"/cast [combat] Berserk",
-"/castsequence Cenarion ward",
-KeyRelease = [[
-/startattack
-]],
-}
-
-Sequences['SAM_bear2'] = {
-specID = 104,
-author = "John Mets www.wowlazymacros.com",
-helpTxt = " Talents: 2331111",
-KeyPress = [[
-/targetenemy [noharm][dead]
-]],
-"/castsequence Thrash, Thrash, Thrash, Pulverize",
-"/castsequence reset=5 Savage Defense, Swipe, Swipe, Frenzied Regeneration",
-"/castsequence [combat] reset=target Moonfire, Mass Entanglement",
-"/cast !Mangle",
-"/castsequence reset=12 Maul",
-"/cast Survival Instincts",
-"/cast Thrash",
-"/castsequence Swipe, Moonfire, Maul, Mangle",
-"/cast Thrash",
-"/castsequence Swipe, Moonfire, Maul, Mangle",
-"/cast Thrash",
-"/cast Pulverize",
-"/cast Incapacitating Roar",
-"/castsequence [combat] reset=60 Barkskin",
-"/castsequence [combat] reset=180 Berserk",
-"/castsequence reset=30 cenarion ward",
-KeyRelease = [[
-/startattack
-]],
+Sequences['SAM_Bear'] = {
+SpecID = 104,
+Author = "John Mets www.wowlazymacros.com",
+Talents = "2,3,3,1,1,1,1",
+Default=1,
+MacroVersions = {
+  [1] = {
+    KeyPress = {
+      "/targetenemy [noharm][dead]",
+    },
+    StepFunction = "Priority",
+    "/castsequence reset=combat Thrash, Moonfire, Maul, Swipe",
+    "/castsequence reset=combat Savage Defense, Swipe, Swipe, Savage Defense ,Frenzied Regeneration, Ironfur",
+    "/cast Survival Instincts",
+    "/cast Thrash",
+    "/castsequence reset=combat Swipe, Moonfire, Maul, Mangle, Ironfur",
+    "/cast Pulverize",
+    "/cast Incapacitating Roar",
+    "/cast [combat] Barkskin",
+    "/cast [combat] Mighty Bash",
+    "/cast [combat] Berserk",
+    "/castsequence Cenarion ward",
+    KeyRelease={
+      "/startattack",
+    },
+  },
 }
 
 Sequences['SAM_Feral-ST'] = {
-specID = 103,
-author = "Jimmy www.wowlazymacros.com",
-helpTxt = "2231123",
-StepFunction = "Priority",
-KeyPress = [[
-/targetenemy [noharm][dead]
-/castsequence [@player,nostance:2] Cat Form
-/cast [nostealth,nocombat] Prowl
-/stopattack [stealth]
-]],
-'/castsequence [combat,nostealth] Rake,Shred,Shred,Rake,Shred,Rip',
-'/castsequence [combat,nostealth] Shred,Rake,Shred,Shred,Rake,Ferocious Bite',
-KeyRelease = [[
-/startattack
-/cast Tiger's Fury
-]],
+  SpecID = 103,
+  Author = "Jimmy www.wowlazymacros.com",
+  Talents = "2,2,3,1,1,2,3",
+  Default=1,
+  MacroVersions = {
+    [1] = {
+      StepFunction = "Priority",
+      KeyPress = {
+        "/targetenemy [noharm][dead]",
+        "/cast [@player,nostance:2] Cat Form",
+        "/cast [nostealth,nocombat] Prowl",
+        "/stopattack [stealth]"
+      },
+      '/castsequence [combat,nostealth] Rake,Shred,Shred,Rake,Shred,Rip',
+      '/castsequence [combat,nostealth] Shred,Rake,Shred,Shred,Rake,Ferocious Bite',
+      KeyRelease={
+        "/startattack",
+        "/cast Tiger's Fury"
+      },
+    }
+  },
 }
+
 
 Sequences['SAM_Feral-AoE'] = {
-specID = 103,
-author = "Jimmy www.wowlazymacros.com",
-helpTxt = "2231123",
-StepFunction = "Priority",
- KeyPress = [[
-/targetenemy [noharm][dead]
-/castsequence [@player,nostance:2] Cat Form
-/cast [nostealth,nocombat] Prowl
-/stopattack [stealth]
-]],
-'/castsequence [combat,nostealth] Thrash,Swipe,Swipe,Thrash,Swipe,Rip',
-'/castsequence [combat,nostealth] Swipe,Thrash,Swipe,Swipe,Thrash,Ferocious Bite',
-KeyRelease = [[
-/startattack
-/cast Tiger's Fury
-]],
-}
-
-Sequences['SAM_feralaoe'] = {
-specID = 103,
-author = "lloskka www.wowlazymacros.com",
-helpTxt = "Talents 2331223",
-KeyPress = [[
-/targetenemy [noharm][dead]
-/use [noform:2]Cat Form
-/cast [nostealth,nocombat] Prowl
-]],
-[[/cast [combat] !Incarnation: King of the Jungle]],
-[[/cast [combat] !Mighty Bash]],
-[[/castsequence reset=combat Rake,Thrash,Swipe,Swipe,Swipe,Ferocious Bite]],
-KeyRelease = [[
-/cast [combat] !survival Instincts
-/cast [combat] !Tiger's Fury
-/startattack
-]],
-}
-
-Sequences['SAM_feralsingle'] = {
-specID = 103,
-author = "lloskka www.wowlazymacros.com",
-helpTxt = "Talents 2331223",
-KeyPress = [[
-/cast Wild Charge
-/targetenemy [noharm][dead]
-]],
-'/castsequence reset=combat Rake,shred,shred,shred,!Rip,Rake,shred,shred,shred,ferocious bite,Rake,shred,shred,shred,ferocious bite',
-'/use [combat] Berserk',
-'/use [combat] survival instincts',
-'/cast Incarnation: King of the Jungle',
-KeyRelease = [[
-/startattack
-/cast Tiger's Fury
-]],
+  SpecID = 103,
+  Author = "Jimmy www.wowlazymacros.com",
+  Talents = "2,2,3,1,1,2,3",
+  Default=1,
+  MacroVersions = {
+    [1] = {
+      StepFunction = "Priority",
+      KeyPress = {
+        "/targetenemy [noharm][dead]",
+        "/cast [@player,nostance:2] Cat Form",
+        "/cast [nostealth,nocombat] Prowl",
+        "/stopattack [stealth]"
+      },
+      '/castsequence [combat,nostealth] Thrash,Swipe,Swipe,Thrash,Swipe,Rip',
+      '/castsequence [combat,nostealth] Swipe,Thrash,Swipe,Swipe,Thrash,Ferocious Bite',
+      KeyRelease={
+        "/startattack",
+        "/cast Tiger's Fury"
+      },
+    }
+  },
 }
 
 Sequences['SAM_Boomer'] = {
-specID = 102,
-author = "Draik",
-helpTxt = "2323112",
- KeyPress = [[
-/targetenemy [noharm][dead]
-/use [noform]!Moonkin Form
-]],
-'/cast Moonfire',
-'/cast Sunfire',
-'/castsequence [combat] Solar Wrath,Lunar Strike,Solar Wrath,Lunar Strike,Solar Wrath,Solar Wrath',
-'/cast Starsurge',
-KeyRelease = [[
-/startattack
-]],
-}
-
-
-Sequences['SAM_RestoBoomer'] = {
-specID = 105,
-author = "Draik",
-helpTxt = "2312232",
- KeyPress = [[
-/targetenemy [noharm][dead]
-/use [noform]!Moonkin Form
-]],
-'/cast Moonfire',
-'/cast Sunfire',
-'/castsequence [combat] Solar Wrath,Lunar Strike,Solar Wrath,Lunar Strike,Solar Wrath,Solar Wrath',
-'/cast Starsurge',
-KeyRelease = [[
-/startattack
-]],
+  SpecID = 102,
+  Author = "TimothyLuke",
+  Talents = "2,3,2,3,1,1,2",
+  Default=1,
+  MacroVersions = {
+    [1] = {
+      KeyPress = {
+        "/targetenemy [noharm][dead]",
+        "/use [noform]!Moonkin Form"
+      },
+      '/cast Moonfire',
+      '/cast Sunfire',
+      '/castsequence [combat] Solar Wrath,Lunar Strike,Solar Wrath,Lunar Strike,Solar Wrath,Solar Wrath',
+      '/cast Starsurge',
+      KeyRelease={
+        "/startattack",
+      },
+    },
+  }
 }
 
 Sequences["SAM_druid_bala_st"] = {
-specID = 102,
-author="someone",
-helpTxt = "3333132 CTRL Blessing of the Ancients, Shift Celestial Alignment, Alt Solar Beam",
-StepFunction = "Priority",
-KeyPress = [[
-/targetenemy [noharm][dead]
-/cast [noform]!Moonkin Form
-/cast [mod:ctrl] Blessing of the Ancients
-/cast [mod:shift] Celestial Alignment
-/cast [mod:alt] Solar Beam
-]],
-"/castsequence reset=target Sunfire,null",
-"/castsequence reset=target Moonfire,null",
-"/castsequence [combat]Starsurge,Solar Wrath,Lunar Strike,Solar Wrath",
-"/castsequence Lunar Strike,Solar Wrath,Starsurge,Solar Wrath,Lunar Strike,Starsurge",
-"/castsequence [combat]Solar Wrath,Lunar Strike,Solar Wrath,Moonfire",
-"/castsequence [combat]Solar Wrath,Starsurge,Lunar Strike,Solar Wrath",
-"/castsequence [combat]Starsurge,Solar Wrath,Solar Wrath,Sunfire",
-"/castsequence [combat]Solar Wrath,Lunar Strike,Starsurge,Moonfire",
-"/castsequence [combat]Lunar Strike,Solar Wrath,Lunar Strike",
-"/cast Starsurge",
-KeyRelease = [[
-/startattack
-]],
+  SpecID = 102,
+  Author="someone",
+  Talents = "3,3,3,3,1,3,2",
+  Help = "CTRL Blessing of the Ancients, Shift Celestial Alignment, Alt Solar Beam",
+  Default=1,
+  MacroVersions = {
+    [1] = {
+      StepFunction = "Priority",
+      KeyPress = {
+        "/targetenemy [noharm][dead]",
+        "/cast [noform]!Moonkin Form",
+        "/cast [mod:ctrl] Blessing of the Ancients",
+        "/cast [mod:shift] Celestial Alignment",
+        "/cast [mod:alt] Solar Beam",
+      },
+      "/castsequence reset=target Sunfire,null",
+      "/castsequence reset=target Moonfire,null",
+      "/castsequence [combat]Starsurge,Solar Wrath,Lunar Strike,Solar Wrath",
+      "/castsequence Lunar Strike,Solar Wrath,Starsurge,Solar Wrath,Lunar Strike,Starsurge",
+      "/castsequence [combat]Solar Wrath,Lunar Strike,Solar Wrath,Moonfire",
+      "/castsequence [combat]Solar Wrath,Starsurge,Lunar Strike,Solar Wrath",
+      "/castsequence [combat]Starsurge,Solar Wrath,Solar Wrath,Sunfire",
+      "/castsequence [combat]Solar Wrath,Lunar Strike,Starsurge,Moonfire",
+      "/castsequence [combat]Lunar Strike,Solar Wrath,Lunar Strike",
+      "/cast Starsurge",
+      KeyRelease={
+        "/startattack",
+      },
+    },
+  }
 }
 
 Sequences['SAM_KTNDRUHEALS'] = {
-specID = 105,
-author = "KTN",
-helpTxt = "2113112",
-KeyPress = [[
-/cast [@focus,dead] Rebirth
-]],
-'/castsequence [@focus] reset=15/combat Lifebloom, Regrowth, Rejuvenation',
-'/cast [@focus] Cenarion Ward',
-'/castsequence reset=target [@mouseover,exists,help,nodead] Regrowth, Rejuvenation, Healing Touch, Swiftmend',
-KeyRelease = [[
-/cast [@focus]Ironbark
-/cast [@player]Barkskin
-]],
-}
-
-Sequences['SAM_KTNDRUAOEHEALS'] = {
-specID = 105,
-author = "KTN",
-helpTxt = "2113112",
-'/castsequence [@focus] Wild Growth',
-}
-
-Sequences['SAM_KTNRestoBoom'] = {
-specID = 105,
-author = "KTN",
-helpTxt = "Talents-2113112",
-KeyPress = [[
-/cast [@focus,dead] Rebirth
-/assist [@focus,exists,nodead]
-/targetenemy [noharm]
-/use [noform]!Moonkin Form
-]],
-'/castsequence [combat] Moonfire, Sunfire, Solar Wrath, Lunar Strike, Solar Wrath, Lunar Strike, Solar Wrath, Lunar Strike',
-'/cast Starsurge',
-KeyRelease = [[
-/startattack
-]],
+  SpecID = 105,
+  Author = "KTN",
+  Talents = "2,1,1,3,1,1,2",
+  Default=1,
+  MacroVersions = {
+    [1] = {
+      KeyPress = {
+        "/cast [@focus,dead] Rebirth",
+      },
+      '/castsequence [@focus] reset=15/combat Lifebloom, Regrowth, Rejuvenation',
+      '/cast [@focus] Cenarion Ward',
+      '/castsequence reset=target [@mouseover,exists,help,nodead] Regrowth, Rejuvenation, Healing Touch, Swiftmend',
+      KeyRelease = {
+        "/cast [@focus]Ironbark",
+        "/cast [@player]Barkskin",
+      },
+    },
+  }
 }
