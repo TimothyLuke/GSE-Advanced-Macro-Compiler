a0f6615cbc0e198148e342b1f3cc66c6a2a2ccf3
Timothy Minahan <timothy@minahan.net>
2017-01-16 08:47:26 +1030

    Add pet commands to list #213

--- GSE/API/Statics.lua
+++ GSE/API/Statics.lua
@@ -3,7 +3,7 @@ local Statics = GSE.Static
 local L = GSE.L
 GSELibrary = {}
 
-Statics.CastCmds = { use = true, cast = true, spell = true, cancelaura = true, cancelform = true, stopmacro = true }
+Statics.CastCmds = { use = true, cast = true, spell = true, cancelaura = true, cancelform = true, stopmacro = true, petautocastoff = true, petautocaston = true, petattack = true }
 
 Statics.CleanStrings = {
   [1] = "/console Sound_EnableSFX 0%;",
