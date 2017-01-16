5483cb5d4ec07aee762af4255e5125ae221a8064
Timothy Minahan <timothy@minahan.net>
2017-01-17 09:31:04 +1030

    take 2 #219

--- GSE/API/translator.lua
+++ GSE/API/translator.lua
@@ -176,7 +176,7 @@ function GSE.TranslateString(instring, fromLocale, toLocale, cleanNewLines)
   if string.sub(output, string.len(output)-1) == ", " then
     output = string.sub(output, 1, string.len(output)-2)
   end
-  return string.gsub("\\s+", " ", output)
+  return string.gsub(output, "  ", " " )
 end
 
 function GSE.TranslateSpell(str, fromLocale, toLocale, cleanNewLines)
