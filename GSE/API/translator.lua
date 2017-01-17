6c446d94160862ed17ec3065721b20775bf527b1
Timothy Minahan <timothy@minahan.net>
2017-01-17 13:28:49 +1030

    working through ;'s #219

--- GSE/API/translator.lua
+++ GSE/API/translator.lua
@@ -154,7 +154,9 @@ function GSE.TranslateString(instring, fromLocale, toLocale, cleanNewLines)
             local foundspell, returnval = GSE.TranslateSpell(uetc, fromLocale, toLocale, (cleanNewLines and cleanNewLines or false))
             output = output ..  GSEOptions.KEYWORD .. returnval .. Statics.StringReset .. ", "
           end
+          output = string.sub(output, 1, resetleft -1) .. ";"
         end
+        output = string.sub(output, 1, resetleft -1) 
         local resetleft = string.find(output, ", , ")
         if not GSE.isEmpty(resetleft) then
           output = string.sub(output, 1, resetleft -1)
