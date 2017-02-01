a00788be8adcd605c5089a36a2a51170dfc68d18
Timothy Minahan <timothy@minahan.net>
2017-02-01 17:48:49 +1030

    code cleanup #236

--- GSE/API/Storage.lua
+++ GSE/API/Storage.lua
@@ -715,7 +715,7 @@ end
 
 
 function GSE.CreateMacroString(macroname)
-  return string.format("/click [button:2] %s RightButton ; [button:3] %s MiddleButton; [button:4] %s Button4;[button:5] %s Button5; %s", macroname, macroname, macroname, macroname, macroname)
+  return string.format("#showtooltip\n/click [button:2] %s RightButton; [button:3] %s MiddleButton; [button:4] %s Button4; [button:5] %s Button5; %s", macroname, macroname, macroname, macroname, macroname)
 end
 
 function GSE.UpdateMacroString()
