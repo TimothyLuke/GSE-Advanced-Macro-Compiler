d9f3e7289047559cef93852152111581217efb64
TimothyLuke <timothy@minahan.net>
2017-06-19 14:22:21 +0930

    Fix for #319

--- GSE/GUI/Editor.lua
+++ GSE/GUI/Editor.lua
@@ -142,7 +142,7 @@ function GSE.GUIEditorPerformLayout(frame)
   local transbutton = AceGUI:Create("Button")
   transbutton:SetText(L["Send"])
   transbutton:SetWidth(150)
-  transbutton:SetCallback("OnClick", function() GSE.GUIShowTransmissionGui(editframe.classid.. "," ..editframe.SequenceName) end)
+  transbutton:SetCallback("OnClick", function() GSE.GUIShowTransmissionGui(editframe.ClassId.. "," ..editframe.SequenceName) end)
 
   local editButtonGroup = AceGUI:Create("SimpleGroup")
   editButtonGroup:SetWidth(602)
