820333c32f084a951584bd23eda30bbf22f29ad7
TimothyLuke <timothy@minahan.net>
2017-08-17 13:52:05 +0930

    post development for comments for #335

--- GSE/API/Storage.lua
+++ GSE/API/Storage.lua
@@ -527,7 +527,7 @@ function GSE.OOCUpdateSequence(name,sequence)
       pmcount = table.getn(tempseq.PreMacro) + 1
       gsebutton:SetAttribute('loopstart', pmcount)
       for k,v in ipairs(tempseq.PreMacro) do
-        if GSE.isEmpty(string.find(v, '--')) then
+        if GSE.isEmpty(string.find(v, '--', 1, true)) then
           table.insert(executionseq, v)
         end
       end
@@ -535,7 +535,7 @@ function GSE.OOCUpdateSequence(name,sequence)
     end
 
     for k,v in ipairs(tempseq) do
-      if GSE.isEmpty(string.find(v, '--')) then
+      if GSE.isEmpty(string.find(v, '--', 1, true)) then
         table.insert(executionseq, v)
       end
     end
@@ -544,7 +544,7 @@ function GSE.OOCUpdateSequence(name,sequence)
 
     if not GSE.isEmpty(tempseq.PostMacro) then
       for k,v in ipairs(tempseq.PostMacro) do
-        if GSE.isEmpty(string.find(v, '--')) then
+        if GSE.isEmpty(string.find(v, '--', 1, true)) then
           table.insert(executionseq, v)
         end
       end
