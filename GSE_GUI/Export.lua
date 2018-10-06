afc837d63a956194790a91ceb7a6e72475ab7e67
Timothy Minahan <timothy@minahan.net>
2018-10-06 11:42:41 +0930

    Correct WLM setting for #478

--- GSE_GUI/Export.lua
+++ GSE_GUI/Export.lua
@@ -29,13 +29,7 @@ wlmforumexportcheckbox:SetType("checkbox")
 wlmforumexportcheckbox:SetLabel(L["Format export for WLM Forums"])
 exportframe:AddChild(wlmforumexportcheckbox)
 wlmforumexportcheckbox:SetCallback("OnValueChanged", function (sel, object, value)
-  if value then
-    local exporttext = "`" .. GSE.ExportSequence(GSE.GUIExportframe.sequence, exportframe.sequencename, GSEOptions.UseVerboseExportFormat, "ID", false) .."`"
-    exporttext = exporttext .. GSE.ExportSequenceWLMFormat(GSE.GUIExportframe.sequence, exportframe.sequencename)
-    GSE.GUIExportframe.ExportSequenceBox:SetText(exporttext)
-  else
-    GSE.GUIExportframe.ExportSequenceBox:SetText(GSE.ExportSequence(GSELibrary[tonumber(exportframe.classid)][exportframe.sequencename], exportframe.sequencename, GSEOptions.UseVerboseExportFormat, "ID", false))
-  end
+  GSE.GUIUpdateExportBox()
 end)
 wlmforumexportcheckbox:SetValue( GSEOptions.UseWLMExportFormat)
 
@@ -47,10 +41,10 @@ exportframe:AddChild(enforceCompatabilityCheckbox)
 enforceCompatabilityCheckbox:SetCallback("OnValueChanged", function (sel, object, value)
   if value then
     exportframe.sequence.EnforceCompatability = true
-    exportframe.sequence.GSEVersion = GSE.VersionString
   else
     exportframe.sequence.EnforceCompatability = false
   end
+  exportframe.sequence.GSEVersion = GSE.VersionString
   GSE.GUIUpdateExportBox()
 end)
 
@@ -92,7 +86,7 @@ GSE.GUIExportframe = exportframe
 exportframe.ExportSequenceBox = exportsequencebox
 
 function GSE.GUIUpdateExportBox()
-  if GSEOptions.UseWLMExportFormat then
+  if wlmforumexportcheckbox:GetValue() then
     local exporttext = "`" .. GSE.ExportSequence(GSE.GUIExportframe.sequence, exportframe.sequencename, GSEOptions.UseVerboseExportFormat, "ID", false) .."`"
     exporttext = exporttext .. GSE.ExportSequenceWLMFormat(GSE.GUIExportframe.sequence, exportframe.sequencename)
     GSE.GUIExportframe.ExportSequenceBox:SetText(exporttext)
