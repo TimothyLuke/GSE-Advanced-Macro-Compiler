814e7a9b25182a07dba6b032718e11d0389db5f9
Timothy Minahan <timothy@minahan.net>
2020-10-29 07:59:44 +1030

    #696 Sample macro for Shaman Elemental

--- GSE/API/Init.lua
+++ GSE/API/Init.lua
@@ -8,7 +8,7 @@ GSE.VersionString = GetAddOnMetadata("GSE", "Version");
 
 --@debug@
 if GSE.VersionString:find("version") then
-    GSE.VersionString = "2.6.22-development"
+    GSE.VersionString = "2.6.23-development"
 end
 --@end-debug@
 
