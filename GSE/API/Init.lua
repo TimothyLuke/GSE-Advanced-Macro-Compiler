2406a686e2f29ce92c8aa48db2212fe01692d3a2
Timothy Minahan <timothy@minahan.net>
2020-12-14 16:00:07 +1030

    #696 Feral Druid Sample Macro

--- GSE/API/Init.lua
+++ GSE/API/Init.lua
@@ -8,7 +8,7 @@ GSE.VersionString = GetAddOnMetadata("GSE", "Version");
 
 --@debug@
 if GSE.VersionString:find("version") then
-    GSE.VersionString = "2.6.30-development"
+    GSE.VersionString = "2.6.33-development"
 end
 --@end-debug@
 
