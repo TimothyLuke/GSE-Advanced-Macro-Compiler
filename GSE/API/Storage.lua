a17c43a9904c11c743c857591cf31808101838ba
Timothy Minahan <timothy@minahan.net>
2021-03-22 16:17:04 +1030

    #812 Allow fort MS in Pause Action to contain GCD as a value.

--- GSE/API/Storage.lua
+++ GSE/API/Storage.lua
@@ -1917,7 +1917,12 @@ local function processAction(action, metaData)
         local PauseActions = {}
         local clicks = action.Clicks
         if GSE.isEmpty(clicks) then
-            clicks = math.ceil(action.MS / GSEOptions.msClickRate)
+            local clicks = action.MS
+            if clicks == '~~GCD~~' or clicks == 'GCD' then
+                clicks = GSE.GetGCD()
+            else
+                clicks = math.ceil(clicks / GSEOptions.msClickRate)
+            end
         end
         if clicks > 0 then
             for loop=1,clicks do
