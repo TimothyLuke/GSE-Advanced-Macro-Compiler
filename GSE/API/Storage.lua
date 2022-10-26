d80f0ccd09ada2b343b85069222843666c2505fa
Timothy Minahan <timothy@minahan.net>
2022-10-26 20:55:37 +1030

    #1188 Extraneous ; removed =-= NTOE: macro stubs need to be deleter and "Create Icon" again

--- GSE/API/Storage.lua
+++ GSE/API/Storage.lua
@@ -555,7 +555,7 @@ end
 
 function GSE.CreateMacroString(macroname)
     return string.format(
-        "#showtooltip\n/click [button:1] %s LeftButton;; [button:2] %s RightButton; [button:3] %s MiddleButton; [button:4] %s Button4; [button:5] %s Button5; %s",
+        "#showtooltip\n/click [button:1] %s LeftButton; [button:2] %s RightButton; [button:3] %s MiddleButton; [button:4] %s Button4; [button:5] %s Button5; %s",
         macroname,
         macroname,
         macroname,
