fd25dbd112964a9ed1bc571e194c403d3951aba2
Timothy Minahan <timothy@minahan.net>
2024-07-04 18:18:18 +0930

    #1450 Remove redundant lines from export

--- GSE_GUI/Export.lua
+++ GSE_GUI/Export.lua
@@ -2,9 +2,6 @@ local GSE = GSE
 local Statics = GSE.Static
 local AceGUI = LibStub("AceGUI-3.0")
 local L = GSE.L
-local libS = LibStub:GetLibrary("AceSerializer-3.0")
-local libC = LibStub:GetLibrary("LibCompress")
-local libCE = libC:GetAddonEncodeTable()
 
 local exportframe = AceGUI:Create("Frame")
 exportframe:Hide()
