2650a6f461b48124817fddc4fa93c635cc254116
Timothy Minahan <timothy@minahan.net>
2024-10-01 09:17:52 +0930

    #1656 check for specialisation in Classic

--- GSE/API/CharacterFunctions.lua
+++ GSE/API/CharacterFunctions.lua
@@ -6,8 +6,12 @@ local Statics = GSE.Static
 
 --- Return the characters current spec id
 function GSE.GetCurrentSpecID()
-    local currentSpec = GetSpecialization()
-    return currentSpec and select(1, GetSpecializationInfo(currentSpec)) or 0
+    if GSE.GameMode <= 4 then
+        return GSE.GetCurrentClassID() and GSE.GetCurrentClassID()
+    else
+        local currentSpec = GetSpecialization()
+        return currentSpec and select(1, GetSpecializationInfo(currentSpec)) or 0
+    end
 end
 
 --- Return the current GCD for the current character
