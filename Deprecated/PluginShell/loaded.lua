bf33dc8f086b50fbba00560f93a477a7ae1d3351
Timothy Minahan <timothy@minahan.net>
2025-01-31 12:25:23 +1030

    #1585 update Sample Plugin

--- Deprecated/PluginShell/loaded.lua
+++ Deprecated/PluginShell/loaded.lua
@@ -1,17 +1,42 @@
 local ModName, Sequences = ... -- Sequences will bring in macros sotred in other other files
 local GSE = GSE
+---- PRINT MISSING GSE
+if GSE == nil then
+  print(
+    "Addon requires GSE3. You can get it from Curseforge @https://www.curseforge.com/wow/addons/gse-gnome-sequencer-enhanced-advanced-macros"
+  )
+  return
+end
+
 local L = GSE.L
 local Statics = GSE.Static
 local GSEPlugin = LibStub("AceAddon-3.0"):NewAddon(ModName, "AceEvent-3.0")
 
+-- check in case Sequences is empty
+if GSE.isEmpty(Sequences) then
+  local Sequences = {}
+end
+
 -- This is an example only
 -- Sequences["newMacro"] = [[asdaksjdhfskjdfhgsdfsdf]]
 
-
 --- We make this a function as then we can register for the reload event within GSE
 local function loadSequences(event, arg)
-  if arg == ModName then
-    GSE.ImportCompressedMacroCollection (Sequences)
+  -- by changing the event from "load" you can do other login here if you wish.
+
+  if arg == ModName then -- check this is my mod not someone elses
+    for k, v in pairs(Sequences) do
+      GSE.ImportSerialisedSequence(v, false) -- change this to true to popup the merge dialog instad
+    end
+
+    ---- Tell GSE to reload the new stuff
+    GSE.PerformReloadSequences()
+
+    ---- Print Success Message
+    GSE.Print(
+      "Hello, " .. UnitName("player") .. " " .. UnitLevel("player") .. "  - Holy's Mage Macros has been loaded.",
+      ModName
+    )
   end
 end
 
