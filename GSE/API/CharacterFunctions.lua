cf6b1885ec7837a1e3db7b9ed638a392af9f8d17
Timothy Minahan <timothy@minahan.net>
2025-05-04 04:58:14 +0930

    #1796 Fix PTR Export to handle MoP API differences gracefully.

--- GSE/API/CharacterFunctions.lua
+++ GSE/API/CharacterFunctions.lua
@@ -10,9 +10,10 @@ function GSE.GetCurrentSpecID()
         return GSE.GetCurrentClassID() and GSE.GetCurrentClassID()
     else
         local currentSpec =
-            C_SpecializationInfo.GetSpecialization() and C_SpecializationInfo.GetSpecialization() or GetSpecialization()
+            C_SpecializationInfo and C_SpecializationInfo.GetSpecialization and C_SpecializationInfo.GetSpecialization() or
+            GetSpecialization()
         local currentSpecInfo =
-            C_SpecializationInfo.GetSpecializationInfo(currentSpec) and
+            C_SpecializationInfo and C_SpecializationInfo.GetSpecializationInfo and
             C_SpecializationInfo.GetSpecializationInfo(currentSpec) or
             GetSpecializationInfo(currentSpec)
         return currentSpec and select(1, currentSpecInfo) or 0
