e7abc049e9212eaf2d41b5894e3abe90f171e196
Timothy Minahan <timothy@minahan.net>
2026-06-01 18:54:46 +1000

    #1914 Fix Clearing Keybinds wiping all of GSE_C

--- GSE/API/CharacterFunctions.lua
+++ GSE/API/CharacterFunctions.lua
@@ -181,7 +181,6 @@ function GSE.ClearCommonKeyBinds()
     end
     local char = UnitFullName("player")
     local realm = GetRealmName()
-    GSE_C = {}
     GSE_C["KeyBindings"] = {}
     GSE_C["KeyBindings"][char .. "-" .. realm] = {}
     GSE_C["KeyBindings"][char .. "-" .. realm][tostring(GetSpecialization())] = {}
